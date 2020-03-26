7fbc7d6e879399cec742b39434ece634511c66d6
UrAWizardHarryPotter <zach.schwartzman@gmail.com>
2020-03-25 23:08:35 -0700

    Define enqueue and size... bug in dequeue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  someInstance.storage = storage;
+  var key = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[key] = value;
+    key = key + 1;
   };
 
   someInstance.dequeue = function() {
+    if (key > 0) {
+      delete storage[key];
+      key = key - 1;
+    } else {
+      delete storage[key];
+    }
   };
 
   someInstance.size = function() {
+    return key;
   };
 
   return someInstance;
