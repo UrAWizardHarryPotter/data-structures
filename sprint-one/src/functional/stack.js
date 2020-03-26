8c64d0282f88a1734137092f9a9d9d0201c0acaf
UrAWizardHarryPotter <zach.schwartzman@gmail.com>
2020-03-25 22:36:14 -0700

    Define push, pop and size for Functional Class

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  someInstance.storage = storage;
+  var key = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[key] = value;
+    key = key + 1;
   };
 
   someInstance.pop = function() {
+    if (key > 0) {
+      key = key - 1;
+      return storage[key];
+      delete storage[key];
+    }
   };
 
   someInstance.size = function() {
+    return key;
   };
 
   return someInstance;
