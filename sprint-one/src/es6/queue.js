e108aaf7764c9b66ebf051bd356788cae46a42ab
UrAWizardHarryPotter <zach.schwartzman@gmail.com>
2020-03-26 12:58:40 -0700

    Finish es6Define Queue and Stack for es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,38 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.key = 0;
+    this.beginning = 0;
+    this.storage = {};
+  }
+
+  // enqueue
+  enqueue(value) {
+    this.storage[this.key] = value;
+    //increment key count
+    this.key = this.key + 1;
+  }
+
+  // dequeque
+  dequeue() {
+    // assign beginning element
+    let element = this.storage[this.beginning];
+    // remove lowest element from the storafe
+    delete this.storage[this.beginning];
+    // increment by 1
+    this.beginning = this.beginning + 1;
+    // return element
+    return element;
+  }
+
+  // size
+  size() {
+    var size = this.key - this.beginning;
+    if (size >= 0) {
+      return size;
+    } else {
+      return 0;
+    }
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,28 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.keyClass = 0;
+  }
+
+  // push
+  push(value) {
+    this.storage[this.keyClass] = value;
+    this.keyClass = this.keyClass + 1;
+  }
+
+  // pop
+  pop() {
+    if (this.keyClass > 0) {
+      this.keyClass = this.keyClass - 1;
+      return this.storage[this.keyClass];
+      delete this.storage[this.keyClass];
+    }
+  }
+
+  // size
+  size() {
+    return this.keyClass;
   }
 
 }
