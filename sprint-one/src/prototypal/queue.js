83126dc1f0011a35869730b4cb59a46588aaded9
UrAWizardHarryPotter <zach.schwartzman@gmail.com>
2020-03-26 11:09:33 -0700

    Define prototypal for queue and stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,45 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // Object.create
+  var someInstance = Object.create(queueMethods);
+  var storage = {};
+  someInstance.key = 0;
+  someInstance.beginning = 0;
+  someInstance.storage = storage;
+  return someInstance;
 };
 
-var queueMethods = {};
+// define object keys for methods
+var queueMethods = {
+  // enqueque
+  enqueue: function(value) {
+    this.storage[this.key] = value;
+    //increment key count
+    this.key = this.key + 1;
+  },
+
+  // dequeque
+  dequeue: function() {
+    // assign beginning element
+    var element = this.storage[this.beginning];
+    // remove lowest element from the storafe
+    delete this.storage[this.beginning];
+    // increment by 1
+    this.beginning = this.beginning + 1;
+    // return element
+    return element;
+  },
+
+  // size
+  size: function() {
+    var size = this.key - this.beginning;
+    if (size >= 0) {
+      return size;
+    } else {
+      return 0;
+    }
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // Object.create
+  var someInstance = Object.create(stackMethods);
+  var storage = {};
+  someInstance.storage = storage;
+  someInstance.keyClass = 0;
+  return someInstance;
 };
 
-var stackMethods = {};
+// define object keys for methods
+var stackMethods = {
+  // push
+  push: function(value) {
+    this.storage[this.keyClass] = value;
+    this.keyClass = this.keyClass + 1;
+  },
+  // pop
+  pop: function() {
+    if (this.keyClass > 0) {
+      this.keyClass = this.keyClass - 1;
+      return this.storage[this.keyClass];
+      delete this.storage[this.keyClass];
+    }
+  },
+
+  // size
+  size: function() {
+    return this.keyClass;
+  }
+};
 
 
