f849625147527e205ba493f4a8ac5553f797c24e
UrAWizardHarryPotter <zach.schwartzman@gmail.com>
2020-03-26 11:23:52 -0700

    Define queue.js and stack.js for pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.key = 0;
+  this.beginning = 0;
+  this.storage = {};
 };
 
+// push
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.key] = value;
+  //increment key count
+  this.key = this.key + 1;
+};
+
+// pop
+Queue.prototype.dequeue = function() {
+  // assign beginning element
+  var element = this.storage[this.beginning];
+  // remove lowest element from the storage
+  delete this.storage[this.beginning];
+  // increment by 1
+  this.beginning = this.beginning + 1;
+  // return element
+  return element;
+};
+
+// size
+Queue.prototype.size = function() {
+  var size = this.key - this.beginning;
+  if (size >= 0) {
+    return size;
+  } else {
+    return 0;
+  }
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.keyClass = 0;
+};
+
+// push
+Stack.prototype.push = function(value) {
+  this.storage[this.keyClass] = value;
+  this.keyClass = this.keyClass + 1;
+};
+
+// pop
+Stack.prototype.pop = function() {
+  if (this.keyClass > 0) {
+    this.keyClass = this.keyClass - 1;
+    return this.storage[this.keyClass];
+    delete this.storage[this.keyClass];
+  }
+};
+
+// size
+Stack.prototype.size = function() {
+  return this.keyClass;
 };
 
 
